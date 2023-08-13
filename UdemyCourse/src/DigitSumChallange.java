491d3bd96e429890a05c9f95cce4df130e3ae0cb
Vlad <vlad.nedelcioiu@stud.electro.upb.ro>
2023-08-13 20:19:11 +0300

    DigitSum

---
+++ UdemyCourse/src/DigitSumChallange.java
@@ -0,0 +1,19 @@
+public class DigitSumChallange {
+    public static void main(String[] args) {
+        System.out.println(sumDigits(10000));
+    }
+
+    public static int sumDigits(int number) {
+        int sum = 0;
+        int lastDigit = 0;
+        if (number > 0) {
+            for (int i = 1; i <= 10; i++) {
+                lastDigit = number % 10;
+                number = number / 10;
+                sum += lastDigit;
+            }
+            return sum;
+        } else
+            return -1;
+    }
+}
